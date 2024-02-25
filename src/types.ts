443a68a0e45cb44cce4d56fadca5eb903d9fd918
Tang <tangllty@163.com>
2024-02-25 18:12:01 +0800

    Add doc

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,17 @@
+/**
+ * Options for the SVG sprite generation.
+ */
 export interface SvgOptions {
+  /**
+   * Match SVG files in the project root directory.
+   *
+   * @default src/assets/icons/**\/*.svg
+   */
   pattern?: string | string[]
+  /**
+   * Prefix for the SVG symbol IDs.
+   *
+   * @default icon
+   */
   prefix?: string
 }
