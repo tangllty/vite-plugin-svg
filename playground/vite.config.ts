e574dd0a2746d4ba417aca4b9a685218b4bd43b2
Tang <tangllty@163.com>
2024-02-26 11:09:31 +0800

    Cleanup unused import

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // @ts-ignore
 import { svgIconsPlugin } from '../src/index'
-import ts from 'typescript'
 
 // https://vitejs.dev/config/
 export default defineConfig({
